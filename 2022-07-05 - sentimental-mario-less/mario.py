968657d8d3f46cf0f8702a076ccda246260d3fcc
Sandra111755 <98338882+Sandra111755@users.noreply.github.com>
2022-07-05 17:48:23 -0400

    Add files via upload

---
+++ 2022-07-05 - sentimental-mario-less/mario.py
@@ -0,0 +1,34 @@
+# Imported get_int function from cs50 library
+
+from cs50 import get_int
+
+# Defined main to print out a left-aligned pyramid using the height variable as input
+
+
+def main():
+    height = getheight()
+
+    # Uses a combination of the reversed range function with nested for loops and
+    # print concatenation to print out the spaces and hashes appropriately
+    
+    for spaces in reversed(range(height)):
+        print(" " * spaces, end="")
+        for hashes in range(spaces + 1):
+            print(end="")
+        print("#" * (height - spaces))
+
+# gets height with enforced limits on user entered numbers
+
+
+def getheight():
+    while True:
+        height = get_int("Height: ")
+        if height < 1 or height > 8:
+            continue
+        else:
+            break
+
+    return height
+
+
+main()
